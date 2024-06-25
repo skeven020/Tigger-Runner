453d33dbe39137381dda5452bc9987a5e543fa7b
Aluno <aluno@edu.senac.br>
2024-06-25 16:55:52 -0300

    Acerto do GIT Prof Ruben Lindão

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public GameObject player;
-    public bool GameOver
+    public Animation animPlayer;
+    public bool GameOver;
     // Start is called before the first frame update
     void Start()
     {
-           
+       animPlayer = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W))
+        {
+            animPlayer.SetLayerWheight(2, 1);
+        }
+        else 
+        {
+            animPlayer.SetLayerWheight(2, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            animPlayer.SetLayerWheight(1, 1);
+        }
+        else
+        {
+            animPlayer.SetLayerWheight(1, 0);
+        }
     }
 }
